03d1a590788739f1c2aa88b0a6b90ab60173af71
Subhajit Das <jeetudas95@gmail.com>
2019-05-21 18:39:36 +0530

    Data Plotting Functions Added

--- bts/plot.py
+++ bts/plot.py
@@ -3,51 +3,75 @@ import matplotlib.gridspec as gridspec
 import numpy as np
 
 
-def result(image, mask, output,transparency=0.35,save_path=None):
-    """This method plots the original image,activation map of the bottleneck layer
-    and reconstructed image.
-
-    Keyword Arguments:
-    original   - Numpy array of the original data (shape = (784,))
-    activation - Numpy array of the activation map data (shape = (36,))
-    created    -  Numpy array of the reconstructed data (shape = (784,))
-
+def result(image, mask, output, title, transparency=0.38, save_path=None):
+    """ Plots a 2x3 plot with comparisons of output and original image.
+    Works best with Jupyter Notebook/Lab.
+    Parameters:
+        image(ndarray): Array containing the original image of MRI scan.
+        mask(ndarray): Array containing the original mask of tumor.
+        output(ndarray): Model constructed mask from input image.
+        title(str): Title of the plot to be used.
+        transparency(float): Transparency level of mask on images.
+                             Default: 0.38
+        save_path(str): Saves the plot to the location specified.
+                        Does nothing if None. 
+                        Default: None
+    Return:
+        None
     """
 
-    plt.figure(figsize=(15, 12))
+    fig, axs = plt.subplots(2, 3, sharex=True, sharey=True, figsize=(
+        20, 15), gridspec_kw={'wspace': 0.025, 'hspace': 0.010})
+    fig.suptitle(title, x=0.5, y=0.92, fontsize=20)
 
-    gs = gridspec.GridSpec(4, 8)
-    gs.update(wspace=0.5)
-    ax1 = plt.subplot(gs[0:2, 0:4])
-    ax2 = plt.subplot(gs[0:2, 4:8])
-    ax3 = plt.subplot(gs[2:4, 0:4])
-    ax4 = plt.subplot(gs[2:4, 4:8])
+    axs[0][0].set_title("Original Mask", fontdict={'fontsize': 16})
+    axs[0][0].imshow(mask, cmap='gray')
+    axs[0][0].set_axis_off()
 
-    ax1.set_title("Original Mask", fontdict={'fontsize': 16})
-    ax1.imshow(mask, cmap='gray')
-    ax1.set_axis_off()
+    axs[0][1].set_title("Constructed Mask", fontdict={'fontsize': 16})
+    axs[0][1].imshow(output, cmap='gray')
+    axs[0][1].set_axis_off()
 
-    ax2.set_title("Constructed Mask", fontdict={'fontsize': 16})
-    ax2.imshow(output, cmap='gray')
-    ax2.set_axis_off()
+    mask_diff = np.abs(np.subtract(mask, output))
+    axs[0][2].set_title("Mask Difference", fontdict={'fontsize': 16})
+    axs[0][2].imshow(mask_diff, cmap='gray')
+    axs[0][2].set_axis_off()
 
-    ax3.set_title("Original Image", fontdict={'fontsize': 16})
-    ax3.imshow(image, cmap='gray')
-    ax3.set_axis_off()
-    
+    seg_output = mask*transparency
+    seg_image = np.add(image, seg_output)/2
+    axs[1][0].set_title("Original Segment", fontdict={'fontsize': 16})
+    axs[1][0].imshow(seg_image, cmap='gray')
+    axs[1][0].set_axis_off()
+
     seg_output = output*transparency
-    seg_image = np.add(image,seg_output)/2
-    ax4.set_title("Segmented Image", fontdict={'fontsize': 16})
-    ax4.imshow(seg_image, cmap='gray')
-    ax4.set_axis_off()
-    
+    seg_image = np.add(image, seg_output)/2
+    axs[1][1].set_title("Constructed Segment", fontdict={'fontsize': 16})
+    axs[1][1].imshow(seg_image, cmap='gray')
+    axs[1][1].set_axis_off()
+
+    axs[1][2].set_title("Original Image", fontdict={'fontsize': 16})
+    axs[1][2].imshow(image, cmap='gray')
+    axs[1][2].set_axis_off()
+
+    plt.tight_layout()
+
     if save_path:
-        plt.savefig(save_path)
-    
+        plt.savefig(save_path, dpi=90, bbox_inches='tight')
+
     plt.show()
 
 
 def loss_graph(loss_list, save_plot=None):
+    """ Plots the loss graph from the training history data.
+    Saves it if required.
+    Parameters:
+        loss_list(list): Array of loss values at each epoch of training time.
+        save_plot(str): Saves the plot to the specified location.
+                        Does nothing if None.
+                        Default: None
+    Returns:
+        None
+    """
     plt.figure(figsize=(20, 10))
     plt.title('Loss Function Over Epoch')
     plt.xlabel('Epochs')
